06af7f75b035f902a85d47cc3fa60ac99c7bb696
etvorellana <evalero@uesc.br>
2022-10-11 11:54:04 -0300

    Exercicios 3 e 4, resolvidos em sala de aula, comentados

--- Aula-10_10/exercicio_03.c
+++ Aula-10_10/exercicio_03.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
-
+/*
+    Escreva um programa que peça ao usuário para fornecer dois 
+    números inteiros, obtenha-os da entrada padrão, usando 
+    scanf, e imprima o maior deles seguido das palavras 
+    “e maior”. Se os números foram iguais, imprima a mensagem 
+    “estes números são iguais”. Use apenas o operador 
+    condicional (?).
+*/
 int main(void)
 {
+    int a, b; // declarando as variáveis
+    printf("Entre com dois valores inteiros: \n");
+    printf("a -> ");
+    scanf("%d", &a);
+    printf("b -> ");
+    scanf("%d", &b);
+    printf("Os valores fornecidos são: %d e %d\n", a, b);
+    // uma solução diferente a vista em sala de aula
+    int opc = (a == b) ? 0 : (a > b) ? 1 : -1;
+    (!opc) ? printf("a = %d, b = %d Estes números são iguais\n", a, b):
+        (!(opc + 1)) ?  printf("b = %d é maior\n", b):
+            printf("a = %d é maior\n", a);
+    // Você entende por que esta implementação funciona?
+    // A solução vista em sala de aula
+    (a == b) ? printf("a = %d, b = %d Estes números são iguais\n", a, b):
+        (a > b) ? printf("a = %d é maior\n", a) : printf("b = %d é maior\n", b);
     return 0;
 }
--- Aula-10_10/exercicio_04.c
+++ Aula-10_10/exercicio_04.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
+/*
+    Escreva um programa que leia um número inteiro e informe 
+    se ele é par ou ímpar.
+*/
+
 int main(void)
 {
+    int val;
+    printf("Entre com um valor inteiro: ");
+    scanf("%d", &val);
+    // variações deste implementaçõ
+    (val % 2 == 0) ? printf("%d é par!!!\n", val) : printf("%d é impar!!!\n", val);
+    (val % 2 != 1) ? printf("%d é par!!!\n", val) : printf("%d é impar!!!\n", val);
+    (!(val % 2)) ? printf("%d é par!!!\n", val) : printf("%d é impar!!!\n", val);
+    (val % 2 < 1) ? printf("%d é par!!!\n", val) : printf("%d é impar!!!\n", val);
+    (val % 2 - 1) ? printf("%d é par!!!\n", val) : printf("%d é impar!!!\n", val);
+    // Sugestão:
+    // Se inverter a ordem no operador como colocar a condição
+    // tente criar 5 variações como nos exemplos anteriores
+    //( ... ) ? printf("%d é impar!!!\n", val) : printf("%d é par!!!\n", val);
     return 0;
 }
